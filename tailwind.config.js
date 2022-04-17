cb4483e06b6b86c40be9ba7f1c3945b31c6093b4
Justin Byrne <theskaterjesus@gmail.com>
2022-04-17 13:14:16 +0100

    Added additional tailwind config for the colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     fontFamily: {
       sans: ["Manrope", "sans-serif"]
     },
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          "very-dark-gray": "hsl(217, 19%, 35%)",
+          "desaturated-dark": "hsl(214, 17%, 51%)",
+          "grayish": "hsl(212, 23%, 69%)",
+          "light-grayish": "hsl(210, 46%, 95%)",
+        }
+      },
+    },
   },
   plugins: [],
 }
